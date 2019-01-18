54bf2bcc978753f135bf38ef733c7555b19d455c
ShilongYe <46800885+ShilongYe@users.noreply.github.com>
2019-01-17 19:14:48 -0600

    Change commited

--- hello.c.c
+++ hello.c.c
@@ -1,5 +1,5 @@
 /**
- * Author: Wencheng Bao computer science
+ * Author: Wencheng Bao computer science & Shilong Ye Mathematics
  * Date: 2019/01/17
  *
  * A simple hello world program in C
@@ -10,7 +10,8 @@
 
 int main(int argc, char **argv) {
 
-  printf("Wencheng Bao computer science!\n");
+  printf("Name: Wencheng Bao, Major: Computer science!\n");
+  printf("Name: Shilong Ye,   Major: Mathematics!\n");
 
   return 0;
 }
